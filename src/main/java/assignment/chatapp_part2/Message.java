f746cc45c72c11420179e1b6acd7778ca7b74197
sello763 <sellokgashane2@gmail.com>
2026-05-25 23:07:27 +0200

    MY SIXTH COMMIT

---
+++ src/main/java/assignment/chatapp_part2/Message.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment.chatapp_part2;
+
+/**
+ *
+ * @author rasek
+ */
+class Message {
+    
+    
+    
+}
